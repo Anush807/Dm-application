a5050f13b28069a217b1bd851729bbffc3b66e3c
Anush807 <anushpoojary0101@gmail.com>
2026-02-10 11:53:26 +0530

    fe deploy

--- ui2/marketing-dashboard/src/services/api.ts
+++ ui2/marketing-dashboard/src/services/api.ts
@@ -9,7 +9,7 @@ import {
   InsightResponse,
 } from '../types';
 
-const API_BASE_URL = 'http://localhost:3001';
+const API_BASE_URL = import.meta.env.VITE_API_URL
 
 // Create axios instance without auth headers (we'll add them per request)
 const api = axios.create({
